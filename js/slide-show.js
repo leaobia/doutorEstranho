68613bba9a3f9758a868e8d90f427879edd783f0
unknown <biancapeleao@gmail.com>
2022-12-09 08:59:51 -0300

    galeria almost completed

---
+++ js/slide-show.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const images = [
+    { 'id': '1', 'url':'../img/galeria-img1.png' },
+    { 'id': '2', 'url':'../img/galeria-img2.png' },
+    { 'id': '3', 'url':'../img/galeria-img3.png' },
+    { 'id': '4', 'url':'../img/galeria-img4.png' },
+    { 'id': '5', 'url':'../img/galeria-img5.png' },
+]
+
+const containerItems = document.querySelector('#container-items');
+
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
+    })
+}
+
+loadImages( images, containerItems );
+
+let items = document.querySelectorAll('.item');
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore( lastItem, items[0] );
+    items = document.querySelectorAll('.item');
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
